b02278d4bba05efaade292f3efab2ef430653c85
exsketrix <markb_1984@hotmail.com>
2020-06-05 19:57:00 +0100

    updated to allow user to be part of docker group

--- roles/ansible-role/files/entrypoint.sh
+++ roles/ansible-role/files/entrypoint.sh
@@ -5,6 +5,13 @@ if [[ -v LOCAL_USER_ID ]]; then
 
   echo "Starting with UID : $LOCAL_USER_ID"
   useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user -g root
+
+  if [[ -v LOCAL_DOCKER_GROUP_GID ]]; then
+    echo "Adding docker group with GID : $LOCAL_DOCKER_GROUP_ID"
+    groupadd docker -g $LOCAL_DOCKER_GROUP_GID
+    usermod -a -G docker user
+  fi
+
   export HOME=/home/user
   exec /usr/local/bin/gosu user "$@"
 else
