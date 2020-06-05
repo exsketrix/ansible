a8f2ab5823472fc5b316a73dcc80f6f51a10c24c
exsketrix <markb_1984@hotmail.com>
2020-06-05 19:35:26 +0100

    Updated image to allow it to be run as same user as host user attached playbook volume

--- roles/ansible-role/files/entrypoint.sh
+++ roles/ansible-role/files/entrypoint.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-exec "$@"
+if [[ -v LOCAL_USER_ID ]]; then
+
+  echo "Starting with UID : $LOCAL_USER_ID"
+  useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user -g root
+  export HOME=/home/user
+  exec /usr/local/bin/gosu user "$@"
+else
+  useradd -m ansible -g root
+  export HOME=/home/ansible
+  exec /usr/local/bin/gosu ansible "$@"
+fi
