87ac2fa4f888327e9546a5072ccf0680431314f1
exsketrix <markb_1984@hotmail.com>
2020-06-06 08:58:47 +0100

    updated to remove need to initially run container as root user

--- roles/ansible-role/files/entrypoint.sh
+++ roles/ansible-role/files/entrypoint.sh
@@ -4,23 +4,26 @@ set -e
 if [[ -v LOCAL_USER_ID ]]; then
 
   echo "Starting with UID : $LOCAL_USER_ID"
-  useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user -g root
-
+  #useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user -g root
+  echo "user:x:$LOCAL_USER_ID:0::/home/user:/bin/bash" >> /etc/passwd
   if [[ -v LOCAL_DOCKER_GROUP_GID ]]; then
     echo "Using docker group with GID : $LOCAL_DOCKER_GROUP_GID"
     if (eval "getent group $LOCAL_DOCKER_GROUP_GID"); then
       : # do nothing, it worked
     else
       echo "Adding docker group with GID : $LOCAL_DOCKER_GROUP_GID"
-      groupadd docker -g $LOCAL_DOCKER_GROUP_GID
+      #groupadd docker -g $LOCAL_DOCKER_GROUP_GID
+      echo "docker:x:$LOCAL_DOCKER_GROUP_GID:user" >> /etc/group
     fi
-    usermod -a -G $LOCAL_DOCKER_GROUP_GID user
+    #usermod -a -G $LOCAL_DOCKER_GROUP_GID user
   fi
 
   export HOME=/home/user
-  exec /usr/local/bin/gosu user "$@"
+  exec "$@"
+  #exec /usr/local/bin/gosu user "$@"
 else
-  useradd -m ansible -g root
-  export HOME=/home/ansible
-  exec /usr/local/bin/gosu ansible "$@"
+  #useradd -m ansible -g root
+  #export HOME=/home/ansible
+  #exec /usr/local/bin/gosu ansible "$@"
+  exec "$@"
 fi
