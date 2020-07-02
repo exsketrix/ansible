010e112986d84225423280484312e25bf68791fd
exsketrix <markb_1984@hotmail.com>
2020-07-02 11:42:54 +0100

    Updated python script used to determine latest version to ignore non strict versions

--- roles/ansible-role/files/latest_ansible_version.py
+++ roles/ansible-role/files/latest_ansible_version.py
@@ -1,14 +1,20 @@
 import json
 from urllib import request
 from distutils.version import LooseVersion
+import re
+
+strictversion_regex = re.compile('^\\d+\\.\\d+\\.\\d+$')
+strict_versions = []
 
 
 def latest_version(package_name):
     url = "https://pypi.python.org/pypi/%s/json" % (package_name,)
     data = json.load(request.urlopen(request.Request(url)))
     versions = data["releases"].keys()
-    return sorted(versions, key=LooseVersion)[-1]
-    return list(versions)[-1]
+    for v in versions:
+        if (strictversion_regex.match(v)):
+            strict_versions.append(v)
+    return sorted(strict_versions, key=LooseVersion)[-1]
 
 
 print(latest_version("ansible"), end='')
